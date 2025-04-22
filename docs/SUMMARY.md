f8cbe055ac4d304b08d6e89ba8b58f6f2a943635
Parthiv Seetharaman <parthivs@myrdd.info>
2025-04-21 22:02:29 -0700

    fix: add guides and missing template

--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -1,9 +1,9 @@
 # Summary
 
 - [Introduction](./introduction.md)
-- [Guides](./tutorials/index.md)
-    - [Build Valence Contracts](./tutorials/build-valence-contracts.md)
-    - [Upload Contracts](./tutorials/upload-contracts.md)
+- [Guides](./guides/index.md)
+    - [Build Valence Contracts](./guides/build-valence-contracts.md)
+    - [Upload Contracts](./guides/upload-contracts.md)
 - [Flake Modules Reference](./reference/flake-modules/index.md)
     - [Upload Contracts Options](./reference/flake-modules/upload-contracts.md)
     - [Valence Contracts Options](./reference/flake-modules/valence-contracts.md)
---
+++ templates/build-valence-contracts/flake.nix
@@ -0,0 +1,33 @@
+{
+  nixConfig = {
+    extra-substituters = "https://timewave.cachix.org";
+    extra-trusted-public-keys = ''
+      timewave.cachix.org-1:nu3Uqsm3sikI9xFK3Mt4AD4Q6z+j6eS9+kND1vtznq4=
+    '';
+  };
+
+  description = "Package for valence-protocol contracts.";
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { system, ... }: {
+        valence-contracts.builds = {
+          main = {
+            version = "main";
+            src = inputs.valence-contracts-main;
+          };
+        }
+      };
+    };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    zero-nix.url = "github:timewave-computer/zero.nix";
+    valence-contracts-main.url = "github:timewave-computer/valence-protocol";
+    valence-contracts-main.flake = false;
+  };
+}
